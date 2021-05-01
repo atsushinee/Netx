97f9d6ab8cb28e056a3665f593c49234b620af13
atsus <atsushinee@outlook.com>
2021-05-01 11:38:36 +0800

    auto commit date

--- Netx/commit.bat
+++ Netx/commit.bat
@@ -1,7 +1,7 @@
 @echo off
 for /l %%i in (1,1,30) do (
-    date 2021/4/%%i
-    echo 2021/4/%%i > .Netx
+    date 2021/5/%%i
+    echo 2021/5/%%i > .Netx
     git add .
     git commit -m "auto commit date"
 )
