a135927972dad70909f7ba7d603f29057f2d0cc3
atsus <atsushinee@outlook.com>
2022-07-01 12:01:00 +0800

    auto commit date

--- Netx/commit.bat
+++ Netx/commit.bat
@@ -1,7 +1,7 @@
 @echo off
 for /l %%i in (1,1,30) do (
-    date 2022/6/%%i
-    echo 2022/6/%%i > .Netx
+    date 2022/7/%%i
+    echo 2022/7/%%i > .Netx
     git add .
     git commit -m "auto commit date"
 )
