66554a8a35e27033c973237ccb21a563139b9593
atsus <atsushinee@outlook.com>
2022-07-26 16:36:58 -0700

    update

--- Netx/Properties/Resources.Designer.cs
+++ Netx/Properties/Resources.Designer.cs
@@ -11,14 +11,6 @@
 namespace Netx.Properties
 {
 
-
-    /// <summary>
-    ///   强类型资源类，用于查找本地化字符串等。
-    /// </summary>
-    // 此类是由 StronglyTypedResourceBuilder
-    // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
-    // 若要添加或删除成员，请编辑 .ResX 文件，然后重新运行 ResGen
-    // (以 /str 作为命令选项)，或重新生成 VS 项目。
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
