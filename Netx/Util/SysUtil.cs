1b42ebdac519457471a94eae6673d20395385056
atsus <atsushinee@outlook.com>
2022-09-04 16:36:32 -0700

    add

---
+++ Netx/Util/SysUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Netx.Util
+{
+    public class SysUtil
+    {
+         
+
+        public static void Main(string[] args)
+        {
+          
+        }
+
+    
+    }
+}
